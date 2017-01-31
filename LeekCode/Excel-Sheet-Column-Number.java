06d5a8449d28108bc46879d4b0ce46f84efeae25
vchakh <vchakhmatov@gmail.com>
2017-01-31 15:00:27 -0800

    Create Excel-Sheet-Column-Number.java

---
+++ LeekCode/Excel-Sheet-Column-Number.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int titleToNumber(String s) {
+        String column = s;
+		int result = 0;
+		
+		//Note: 'A'=65; 'Z'=90; ('Z'-'A') = 25;
+		for (int i = 0; i<column.length(); ++i){
+			result *=26;
+			result +=column.charAt(i) - 'A' + 1;
+		}
+		
+		System.out.println("column="+column+"; result="+result);
+		return result;
+    }
+}
