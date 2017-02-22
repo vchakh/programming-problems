5e63ec76aa8a24b7a586e416ba904ec91ff0f0ca
vchakh <vchakhmatov@gmail.com>
2017-02-22 15:46:31 -0800

    Update findAnagrams_438.java

--- LeetCode/findAnagrams_438.java
+++ LeetCode/findAnagrams_438.java
@@ -1,54 +1,44 @@
 	//438. Find All Anagrams in a String
 	//Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
 	// https://leetcode.com/problems/find-all-anagrams-in-a-string/
-	//Input: s: "cbaebabacd" p: "abc"
+	//Input: s: "cbaebabacd" p: "abc"; ("abab", "ab");("cbaebabacd", "abc");("af","be");("cbaebabacd", "abc")
 	//Output: [0, 6]
 	//Explanation:
 	//   The substring with start index = 0 is "cba", which is an anagram of "abc".
 	//   The substring with start index = 6 is "bac", which is an anagram of "abc".
 	public static List<Integer> findAnagrams_438(String s, String p) {
 		List<Integer> ls = new ArrayList<Integer>();
+		System.out.println("Input string s="+s+"; search input target substring p="+p);
+		
 		if(s.equals(p)){
 			ls.add(new Integer(1));
 			return ls; 
 		} else if(p.length()>s.length()){
-			//ls.add(0);
 			return ls;			
-		}
-			
+		} 
+		
+		//Sort input target substring
+		char[] arr_target = p.toCharArray();
+		Arrays.sort(arr_target);
 		int num = p.length();
 		
-		//Sort target string
-		String[] arr_target = p.split("");
-		Arrays.sort(arr_target);
-		String sorted_target = "";
-		for (int i=0; i<arr_target.length; ++i){
-			sorted_target +=arr_target[i];
-		}
-		System.out.println("sorted: "+sorted_target);
-		
+		//Find search occurrence in input string 
 		for (int i=0; i<=(s.length()-num); ++i){
 			String sub = s.substring(i, i+num);
-			//System.out.println("i="+i+" num="+num +"   sub="+sub); 
+			char[] arr_sub = sub.toCharArray();
+			Arrays.sort(arr_sub);
 			
-			//Sort input string
-			String[] arr = sub.split("");
-			Arrays.sort(arr);
-			String sorted = "";
-			for (int j=0; j<arr.length; ++j){
-				sorted +=arr[j];
-			}
-			//System.out.println("sorted =" +sorted);
-			if(sorted_target.equals(sorted)){
-				System.out.println(" xyi = "+i);
+			if (Arrays.equals(arr_target, arr_sub)){
 				ls.add(new Integer(i));
-			}
+				System.out.println("Index matched i="+i);
+			}	
 		}
-		
+
 		System.out.println("FINAL !!!!");
-		Iterator iter = ls.iterator();
+		Iterator<Integer> iter = ls.iterator();
 		while(iter.hasNext()){
 			System.out.println(iter.next()+",");
 		}
+
 		return ls;
 	}
